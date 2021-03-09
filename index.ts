83e8c80b8a6866392c22cd59cc7d47257086c6d7
worsrp <sirapop_p@cmu.ac.th>
2021-03-09 15:42:42 +0700

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import fs from 'fs'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { body, query, validationResult } from 'express-validator'
+import e from 'express'
 
 const app = express()
 app.use(bodyParser.json())
@@ -17,32 +19,77 @@ interface JWTPayload {
   password: string;
 }
 
-app.post('/login',
-  (req, res) => {
+interface DbSchema {
+  users: User[]
+}
 
+interface User {
+  id: number
+  username: string
+  password: string
+  firstname: string
+  lastname: string
+  balance: number
+}
+
+interface schema {
+  users: User[]
+}
+
+type LoginArgs = Pick<User, 'username' | 'password'>
+app.post<any, any, LoginArgs>('/login', async (req, res) => {
     const { username, password } = req.body
     // Use username and password to create token.
-
+    const raw = fs.readFileSync('db.json', 'utf8')
+    const db: DbSchema = JSON.parse(raw)
+    const user = await db.users.find(user => user.username === req.body.username) 
+    if (!user) { // if input username does not has in the data base
+      res.status(400).json({ message: 'Invalid username or password' })
+      return
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) { // if input password does not has in the data base
+      res.status(400).json({ message: 'Invalid username or password' })
+      return
+    }
+    const token = jwt.sign({ username: user.username, password: user.password }, SECRET) as string// token declaration
+    console.log(token)
     return res.status(200).json({
       message: 'Login succesfully',
+      token: token
     })
   })
 
-app.post('/register',
-  (req, res) => {
-
+  type RegisterArgs = Omit<User, 'id'>
+app.post<any, any, RegisterArgs>('/register', (req, res) => {
     const { username, password, firstname, lastname, balance } = req.body
+    const raw = fs.readFileSync('db.json', 'utf8')
+    const db: DbSchema = JSON.parse(raw)
+    const hashPassword = bcrypt.hashSync(req.body.password, 10)
+    const usrname = db.users.find(user => user.username === req.body.username);
+    if(usrname){
+      res.status(400).json({ message: 'Username is already in used' })
+      return
+    }
+    db.users.push({
+      ...{ username, password, firstname, lastname, balance },
+      id: Date.now(),             // use current date as id  
+      password: hashPassword,
+    })
+    fs.writeFileSync('db.json', JSON.stringify(db))
+    res.status(200).json({ message: 'Register complete' })
   })
 
 app.get('/balance',
   (req, res) => {
-    const token = req.query.token as string
+    const token = req.query.token as string || req.body.token as string
+    console.log(token)
     try {
       const { username } = jwt.verify(token, SECRET) as JWTPayload
-  
+      res.status(200).json({ data: username })
     }
     catch (e) {
       //response in case of invalid token
+      res.status(401).json({ message: 'Invalid token' , emess: e.message})
     }
   })
 
@@ -52,7 +99,7 @@ app.post('/deposit',
 
     //Is amount <= 0 ?
     if (!validationResult(req).isEmpty())
-      return res.status(400).json({ message: "Invalid data" })
+      return res.status(400).json({ message: "Invalid data"})
   })
 
 app.post('/withdraw',
@@ -62,14 +109,17 @@ app.post('/withdraw',
 app.delete('/reset', (req, res) => {
 
   //code your database reset here
-  
+  const raw = fs.readFileSync('db.json', 'utf8')
+  const db: DbSchema = JSON.parse(raw)
+  db.users.splice(0,db.users.length)
+  fs.writeFileSync('db.json', JSON.stringify(db))
   return res.status(200).json({
     message: 'Reset database successfully'
   })
 })
 
 app.get('/me', (req, res) => {
-  
+  res.status(200).json({ firstname: 'Sirapop' , lastname: 'Para', code: 620610815, gpa: 3.57})
 })
 
 app.get('/demo', (req, res) => {
@@ -77,5 +127,7 @@ app.get('/demo', (req, res) => {
     message: 'This message is returned from demo route.'
   })
 })
+
+
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
